246dc691bf908dc264234a791f70ae79fc813b1d
wiliamferysagala <wiliamferysagala@gmail.com>
2025-02-14 20:05:28 +0700

    js: h-1

---
+++ javascript/02.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  return function () {
+    return (n += 1);
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
